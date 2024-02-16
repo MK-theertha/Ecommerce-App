8cc015a54da2bef9500df8fc15d6158198e8e50d
MK-theertha <theerthamk1998@gmail.com>
2024-02-16 13:13:34 +0530

    custom error

--- app.js
+++ app.js
@@ -1,15 +1,37 @@
-const express = require("express");
-require("dotenv").config();
+const express = require('express');
+require('dotenv').config();
+const morgan = require('morgan');
+const cookieParser = require('cookie-parser');
+const fileUpload = require('express-fileupload');
 
 const app = express();
 
-//import all routes 
-const home = require("./routes/home");
+//for swagger documentation
+const swaggerUi = require('swagger-ui-express');
+const YAML = require('yamljs');
+const swaggerDocument = YAML.load('./swagger.yaml');
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+
+//regular middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+//cookies and file middleware
+app.use(cookieParser());
+app.use(
+  fileUpload({
+    useTempFiles: true,
+    tempFileDir: '/tmp/',
+  })
+);
+
+//morgan middleware
+app.use(morgan('tiny'));
+
+//import all routes
+const home = require('./routes/home');
 
 //router middleware
-app.use("/api/v1", home);
-
-
-
+app.use('/api/v1', home);
 
 module.exports = app;
--- routes/home.js
+++ routes/home.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
-const { home } = require("../controllers/homeController");
+const { home,homeDummy } = require("../controllers/homeController");
 
 router.route('/').get(home);
+router.route("/dummy").get(homeDummy);
 module.exports = router;
